f279ba224de13aafc7a81a050bb72579f6deb67e
jeeva-res <res.pjeevanantham@gmail.com>
2025-10-29 15:31:13 +0530

    resul sdk updated

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,1 +1,2 @@
-importScripts('https://sdk.smartdx.co/Scripts/gcp-push.js');
+importScripts('https://sdk.resul.io/Scripts/push-sw.js');
+// importScripts('https://sdk.smartdx.co/Scripts/gcp-push.js');
